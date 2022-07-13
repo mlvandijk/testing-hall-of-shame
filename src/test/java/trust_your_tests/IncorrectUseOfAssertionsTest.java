0dfadbefd67dee07f91a961c1698b99d464a4970
maritvandijk <mlvandijk@gmail.com>
2022-07-13 12:07:51 +0200

    How to fix example for Never trust a test you haven't seen fail

--- src/test/java/trust_your_tests/IncorrectUseOfAssertionsTest.java
+++ src/test/java/trust_your_tests/IncorrectUseOfAssertionsTest.java
@@ -11,9 +11,23 @@ class IncorrectUseOfAssertionsTest {
     * This test will never fail.
     */
     @Test
-    void neverTrustATestYouHaventSeenFail() {
+    void incorrectUseOfAssertThat() {
         String x = "x";
 
         assertThat(x);
+    }
+
+    /*
+     * We need to combine assertThat() with an actual assertion.
+     * The test below will fail because the actual and expected String are not the same.
+     * Once we have seen it fail, we can change the expected String to what we expect
+     * and trust that this test will protect us from failures in the future.
+     */
+    @Test
+    void correctUseOfAssertThat() {
+        String actual = "x";
+        String expected = "y";
+
+        assertThat(actual).isEqualTo(expected);
     }
 }

7f49cb58c26ed93584439b0033da01ac938de829
marit.van.dijk <mlvandijk@gmail.com>
2022-07-18 09:16:02 +0200

    Rename examples

--- src/test/java/failures/CompareValues.java
+++ src/test/java/failures/CompareValues.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class Failure1 {
+class CompareValues {
 
     /*
     * While the test below fails when comparing values, the result does not provide as much information as it could.

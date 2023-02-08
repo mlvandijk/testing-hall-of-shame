b6ce7c15676c6ba5ee8258fd2bb386f721724ae9
marit.van.dijk <mlvandijk@gmail.com>
2023-02-08 16:21:33 +0100

    Add example

---
+++ src/test/java/failures/WillNeverFailTest.java
@@ -0,0 +1,13 @@
+package failures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class WillNeverFailTest {
+
+    @Test
+    void aTestThatWillNeverFail() {
+        assertTrue(true);
+    }
+}
